b13c0c70e5877a158cbc847d48db6f95e29b8457
sanjayamal <rajithasanjayamal0918@gmail.com>
2025-06-13 07:46:34 +0530

    Change height

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,7 +1,11 @@
-const Logo = ({ alt = "Logo", className = "", height=36 }) => {
+const Logo = ({ alt = "Logo", className = ""}) => {
   return (
     <div className={`flex items-center justify-center ${className}`}>
-      <img src="/logo.jpeg" alt={alt} className={`h-${height} w-auto object-contain`}/>
+      <img
+        src="/logo.jpeg"
+        alt={alt}
+        className={`h-36 w-auto object-contain`}
+      />
     </div>
   );
 };
